bc23a9f0d1cbc3a17d130099a73934c8b2e60d7d
Adele <thisisadele@yahoo.com>
2019-04-04 12:52:06 -0400

    Update inventory_diff.py

--- inventory_diff.py
+++ inventory_diff.py
@@ -12,3 +12,19 @@ with open("bb_list") as bb_h:
              else:
                  with open("not_in_bb","a") as fh_not:
                      fh_not.write(i)
+                    
+with open("or_list") as or_h:
+     or_sn = or_h.readlines()
+     # converting the list to the lower case
+     or_sn = [ j.lower() for j in or_sn ]
+     open("in_or","w").close()
+     open("not_in_or","w").close()
+     with open("bb_list") as bb_h:
+         for i in bb_h:
+             if i.lower() in or_sn:
+                 with open("in_or", "a") as fh_in_o:
+                     fh_in_o.write(i)
+             else:
+                 with open("not_in_or","a") as fh_not_o:
+                     fh_not_o.write(i)
+
